77af1bb25a27621890e23417971e2f089a4d2199
akk1029 <aungkoko.work@gmail.com>
2021-12-22 21:30:32 +0630

    Update wsgi.py

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = os.path.expanduser('~/django_projects/mysite')
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS-MODULE'] = 'mysite.settings'
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
-application = get_wsgi_application()
+applicaton = StaticFilesHandler(get_wsgi_application())
